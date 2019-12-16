743f9e9550b73bad531bbd8a2c680b92f2e9ef1c
stan <stanlc@sina.com>
2019-12-16 17:35:12 +0800

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,105 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div>
+      <div class="head clearfix">
+        <div style="float:left">
+          <a>预览</a><a>退一步</a><a>进一步</a><a>刷新</a>
+        </div>
+        <div style="float:right">
+          <a>测试</a><a>保存</a><a>关闭</a>
+        </div>
+      </div>
+      <div class="main">
+        <div class="list">
+          <ul>
+            <li>
+              <el-button @click="addComp()" ref="button">输入状态</el-button>
+            </li>
+          </ul>
+        </div>
+        <div class="uiBox">
+          <el-form>
+            <el-form-item
+            v-for="item in form"
+            :key="item.index"
+            >
+            <el-button>{{item.name}}</el-button>
+            </el-form-item>
+          </el-form>
+        </div>
+        <div class="editor">
+          <el-tabs v-model="activeName" @tab-click="handleClick">
+            <el-tab-pane label="全局" name="first">全局</el-tab-pane>
+            <el-tab-pane label="属性" name="second">
+              <el-form>
+                <el-form-item label="组件信息">
+                  <el-input v-model="EditForm.name"></el-input>
+                </el-form-item>
+              </el-form>
+            </el-tab-pane>
+            <el-tab-pane label="高级" name="third">高级</el-tab-pane>
+          </el-tabs>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
 
 <script>
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  }
+  },
+  data() {
+    return {
+      activeName: 'second',
+      form:[
+        
+      ],
+      EditForm:{},
+    }
+  },
+  methods: {
+    handleClick(tab, event) {
+        // console.log(tab, event);
+    },
+    addComp(){
+      this.form.push({
+        name:'开灯'
+      })
+    },
+  },
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .clearfix::after{
+    content:'';
+    display: block;
+    height: 0;
+    clear: both;
+  }
+  .main{
+    background: #e2e6ec;
+    display: flex;
+    justify-content: space-between;
+  }
+  .list{
+    background: #f5f8fa;
+    width: 20%;
+    height: 90vh;
+  }
+  .uiBox{
+    width: 26vw;
+    height: 90vh;
+    background: #111;
+  }
+  .editor{
+    background: #f5f8fa;
+    width: 20%;
+    height: 90vh;
+  }
 </style>
---
+++ src/plugins/element.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import { Button } from 'element-ui'
+import { Table } from 'element-ui'
+import { Tabs,TabPane,Form,FormItem,Input } from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(Button)
+Vue.use(Table)
+Vue.use(Tabs)
+Vue.use(TabPane)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
